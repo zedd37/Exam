adc33226b34e05899b372ec9a625857c7f0eb86b
zedd37 <77849061+zedd37@users.noreply.github.com>
2022-01-13 08:28:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+function showAns()
+{
+document.getElementById("ten").innerHTML="a";
+document.getElementById("pen").innerHTML="p";
+document.getElementById("zen").innerHTML="a";
+}
+
+function disaappearAns()
+{
+document.getElementById("ten").innerHTML=" ";
+document.getElementById("pen").innerHTML=" ";
+document.getElementById("zen").innerHTML=" ";
+}
+
+
+function ree()
+{
+var div = document.getElementById("newpost");
+if (div.style.display == "none")
+{
+div.style.display = "block";
+}
+}
+function lop()
+{
+  document.getElementById("newpost").style.display = "none";
+}
+
+function fee()
+{
+var miv = document.getElementById("oldpost");
+if (miv.style.display == "none")
+{
+miv.style.display = "block";
+}
+}
+function pol()
+{
+  document.getElementById("oldpost").style.display = "none";
+}
+function qwe() {
+  var x = document.getElementById("q1").value = "a";
+}
+function lkj(){
+ document.getElementById("ten").innerHTML = "a";
+}
+function mnb(){
+ document.getElementById("zen").innerHTML = "a";
+}
+
+function asd() {
+  var x = document.getElementById("q2").value = "p";
+}
+function iop(){
+ document.getElementById("pen").innerHTML = "p";
+}
